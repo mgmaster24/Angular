9a830609c890b19576e3c1b2930df5abcbbf5844
Moose <mgmastersonjr@gmail.com>
2021-04-27 22:55:03 -0400

    Add learning app and edit template

---
+++ Angular-The-Complete-Guide-Project/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AwesomeComponent } from './awesome/awesome.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+  declarations: [AppComponent, ServerComponent, ServersComponent, AwesomeComponent, WarningAlertComponent, SuccessAlertComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
