7b6fd5a788b44e3883bd176688600872dec7741c
Moose <mgmastersonjr@gmail.com>
2021-06-23 13:03:58 -0400

    Shopping list changes

--- ShoppingAndRecipeListApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingAndRecipeListApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a test',
+      // tslint:disable-next-line: max-line-length
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+    new Recipe(
+      'A test recipe 2',
+      'This is a test 2',
+      // tslint:disable-next-line: max-line-length
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  onRecipeClicked(event) {}
 }
